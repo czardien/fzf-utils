2bb4eea33ef6ea19b3fe2e01b01a4654178d75be
Adrien Czerny <adrien.czerny@gmail.com>
2020-04-02 17:22:02 +0100

    Added fzfalias function to help navigate aliases

---
+++ fzf_utils.sh
@@ -0,0 +1,14 @@
+function fzfalias() {
+  local cmd height
+  height=20
+  cmd=$(alias |
+	  sed 's/[^ ]* //' |
+	  sed 's/=/?/' |
+	  column -t -s? -N alias,command -T command -c $COLUMNS -d |
+	  fzf --reverse --border=rounded --height=$height --info=hidden|
+	  awk -F '[[:space:]][[:space:]]+' '{print $2}' |
+	  sed "s/'//" |
+	  sed "s/\(.*\)'/\1 /")
+  echo $cmd
+  $($cmd)
+}
